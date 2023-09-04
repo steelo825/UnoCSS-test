923cf1c554f7c30922123ba6f803319b1fff1672
steelo825 <steelo825@outlook.com>
2023-09-04 09:41:17 +0800

    feat(push): push

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import _ from 'lodash'
+import Icon from './icon.png'
+import Data from './data.xml';
+import Notes from './data.csv';
+import './style.css'
+
+function component() {
+    const element = document.createElement('div')
+    element.innerHTML = _.join(['Hello', 'webpack'], '')
+    element.classList.add('hello')
+    const myIcon = new Image()
+    myIcon.src = Icon
+    element.appendChild(myIcon)
+    console.log(Data)
+    console.log(Notes)
+    return element
+}
+
+document.body.appendChild(component())
